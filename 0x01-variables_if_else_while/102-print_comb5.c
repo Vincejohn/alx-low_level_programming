1ed5de52e1ce74e781b03ce5688f0fc65bdda961
Vincent John <vincejohn987@gmail.com>
2022-06-03 15:05:59 -0700

    My fifth combo

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,9 +17,6 @@ int main(void)
 		{
 			putchar((num1 / 10) + '0');
 			putchar((num1 % 10) + '0');
-			putchar(' ');
-			putchar((num2 / 10) + '0');
-			putchar((num2 % 10) + '0');
 
 			if (num1 == 98 && num2 == 98)
 				continue;
