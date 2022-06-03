2ce058a06e8737664f47cc87a2e6e32829f2deeb
Vincent John <vincejohn987@gmail.com>
2022-06-03 15:24:25 -0700

    My fifth task

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,6 +17,9 @@ int main(void)
 		{
 			putchar((num1 / 10) + '0');
 			putchar((num1 % 10) + '0');
+			putchar(' ');
+			putchar((num2 % 10) + '0');
+			putchar((num2 % 10) + '0');
 
 			if (num1 == 98 && num2 == 98)
 				continue;
