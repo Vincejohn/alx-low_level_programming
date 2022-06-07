d6e2ecbdc7522608616dedb55a834b51313c926a
Vincent John <vincejohn987@gmail.com>
2022-06-07 04:34:41 -0700

    My putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point of my code
+ *
+ * Return: Always return 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char p[] = "_putchar";
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(p[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
