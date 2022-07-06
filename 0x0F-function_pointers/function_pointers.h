e94525a659e2748c0ec2a6fb12245c6d7d2dbadd
Vincent John <vincejohn987@gmail.com>
2022-07-06 11:46:17 -0700

    My correction

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,3 +1,5 @@
+#ifndef HEADERGUARD
+#define HEADERGUARD
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
@@ -8,3 +10,4 @@ int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
 int _putchar(char c);
+#endif
